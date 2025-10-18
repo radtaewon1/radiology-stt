e0db4f9516397f7caa697d1e7f3c3938882e5c2b
radtaewon1 <han754@nate.com>
2025-10-19 00:59:12 +0900

    Update radiology-stt_v1.js

--- radiology-stt_v1.js
+++ radiology-stt_v1.js
@@ -10,7 +10,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
-app.use(express.static('.'));
+app.use(express.static(__dirname));
 app.use(express.json());
 
 const upload = multer({ 
